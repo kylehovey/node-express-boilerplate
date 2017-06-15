8e2ee125afd445e6e172503d5f8ed01bf59d2eac
kylehovey <khoveyc@gmail.com>
2017-06-15 10:22:12 -0700

    Added example component.

---
+++ public/js/components/example/example.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Example = function Example() {
+  _classCallCheck(this, Example);
+};
+
+;
